0997a89a7dc85c8ed398a69a580cf32056e569cd
secozzi <secozzi@proton.me>
2025-08-02 14:26:04 +0200

    Move `presentation-core` to animiru

--- presentation-core/src/main/java/tachiyomi/presentation/core/components/material/Constants.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/components/material/Constants.kt
@@ -11,15 +11,25 @@ const val SECONDARY_ALPHA = .78f
 
 class Padding {
 
+    // AM (BROWSE) -->
+    val superLarge = 72.dp
+    // <-- AM (BROWSE)
+
     val extraLarge = 32.dp
 
     val large = 24.dp
 
     val medium = 16.dp
 
+    val mediumSmall = 12.dp
+
     val small = 8.dp
 
     val extraSmall = 4.dp
+
+    // AM (BROWSE) -->
+    val none = 0.dp
+    // <-- AM (BROWSE)
 }
 
 val MaterialTheme.padding: Padding
