Ramsesdb/gexu-legacy-fork

Fix mass migration not using the same search queries as individual migration (#2736)
(cherry picked from commit eb1741788f1b6f76d3e8590527359ad28ccdc7e1)

app/src/main/java/mihon/feature/migration/list/search/SmartSourceSearchEngine.kt
@@ -1,7 +1,6 @@
 package mihon.feature.migration.list.search
 
 import eu.kanade.tachiyomi.source.CatalogueSource
-import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.SManga
 import mihon.domain.manga.model.toDomainManga
 import tachiyomi.domain.manga.model.Manga
@@ -23,6 +22,6 @@ class SmartSourceSearchEngine(extraSearchParams: String?) : BaseSmartSearchEngin
     }
 
     private fun makeSearchAction(source: CatalogueSource): SearchAction<SManga> = { query ->
-        source.getSearchManga(1, query, FilterList()).mangas
+        source.getSearchManga(1, query, source.getFilterList()).mangas
     }
 }

quickdesh/Animiru

(cherry picked from commit 7161bc2e825bdfd66a1829f7dce42bd0570b1008)

app/src/main/java/mihon/feature/migration/list/search/SmartSourceSearchEngine.kt
@@ -1,7 +1,6 @@
 package mihon.feature.migration.list.search
 
 import eu.kanade.tachiyomi.animesource.AnimeCatalogueSource
-import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.SAnime
 import mihon.domain.anime.model.toDomainAnime
 import tachiyomi.domain.anime.model.Anime
@@ -23,6 +22,6 @@ class SmartSourceSearchEngine(extraSearchParams: String?) : BaseSmartSearchEngin
     }
 
     private fun makeSearchAction(source: AnimeCatalogueSource): SearchAction<SAnime> = { query ->
-        source.getSearchAnime(1, query, AnimeFilterList()).animes
+        source.getSearchAnime(1, query, source.getFilterList()).animes
     }
 }