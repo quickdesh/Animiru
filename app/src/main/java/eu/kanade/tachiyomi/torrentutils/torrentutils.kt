e1ed48443a57ae773ac81d2f538f435975e03af9
Satria41 <satriakelana41@gmail.com>
2025-12-29 22:28:03 +0800

    Add files via upload

---
+++ app/src/main/java/eu/kanade/tachiyomi/torrentutils/torrentutils.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.torrentutils
+
+import eu.kanade.tachiyomi.torrentutils.model.TorrentInfo
+
+object TorrentUtils {
+    fun getTorrentInfo(
+        url: String,
+        title: String,
+    ): TorrentInfo {
+        throw Exception("Please KYS! This is a stub!")
+    }
+}
