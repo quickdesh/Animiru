051b3446dec7d376f60500cfd258323a3a38512e
Secozzi <secozzi@proton.me>
2026-08-13 13:35:14 +0200

    Suppress UnstableApiUsage in settings.gradle.kts
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,11 @@ dependencyResolutionManagement {
         }
         // <-- AY
     }
+
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
