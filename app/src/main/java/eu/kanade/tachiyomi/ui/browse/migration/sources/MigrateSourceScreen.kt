9da277eb8dfd045f6e7cd7c2d23000e6c9230326
Secozzi <secozzi@proton.me>
2026-08-28 23:47:40 +0200

    Make MigrateSourceViewModel collect sources only while subscribed
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/MigrateSourceScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/MigrateSourceScreen.kt
@@ -2,8 +2,8 @@
 package eu.kanade.tachiyomi.ui.browse.migration.sources
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -17,7 +17,7 @@ class MigrateSourceScreen : Screen {
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         val viewModel = viewModel<MigrateSourceViewModel>()
-        val state by viewModel.state.collectAsState()
+        val state by viewModel.state.collectAsStateWithLifecycle()
 
         MigrateSourceScreen(
             state = state,
